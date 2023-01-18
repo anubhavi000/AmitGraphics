2a0fa31eab4d5d2e09793a7ae7b25c14effedda7
unknown <ks765005@gmail.com>
2023-01-18 13:17:26 +0530

    chnages

---
+++ resources/views/EntriesForm/index.blade.php
@@ -0,0 +1,194 @@
+@extends("layouts.panel")
+
+@section('content')
+    <style>
+        ::-webkit-scrollbar {
+            /* width: px; */
+        }
+
+        /* Track */
+        ::-webkit-scrollbar-track {
+            background-color: navy;
+        }
+
+        /* Handle */
+        ::-webkit-scrollbar-thumb {
+            background: #888;
+        }
+
+        /* Handle on hover */
+        ::-webkit-scrollbar-thumb:hover {
+            background: #5556;
+        }
+
+        table.dataTable th,
+        table.dataTable td {
+            white-space: nowrap;
+        }
+
+        tbody {
+            border-right: 1px solid #ccc;
+            border-left: 1px solid #ccc;
+        }
+
+        thead {
+            border-right: 1px solid #ccc;
+            border-left: 1px solid #ccc;
+        }
+
+        .pagination>li>a,
+        .pagination>li>span {
+            position: relative;
+            float: left;
+            padding: 6px 12px;
+            margin-left: -1px;
+            line-height: 1.42857143;
+            color: #337ab7;
+            text-decoration: none;
+            background-color: #fff;
+            border: 1px solid #ddd;
+        }
+
+        .pagination>.active>a,
+        .pagination>.active>a:focus,
+        .pagination>.active>a:hover,
+        .pagination>.active>span,
+        .pagination>.active>span:focus,
+        .pagination>.active>span:hover {
+            z-index: 2;
+            color: #fff;
+            cursor: default;
+            background-color: #337ab7;
+            border-color: #337ab7;
+        }
+
+        .pagination>.disabled>a,
+        .pagination>.disabled>a:focus,
+        .pagination>.disabled>a:hover,
+        .pagination>.disabled>span,
+        .pagination>.disabled>span:focus,
+        .pagination>.disabled>span:hover {
+            color: #777;
+            cursor: not-allowed;
+            background-color: #fff;
+            border-color: #ddd;
+        }
+
+        .dropdown-menu.show {
+            padding-left: 10px;
+        }
+
+    </style>
+    <div class="pcoded-content">
+        <!-- [ breadcrumb ] start -->
+        <div class="page-header card" id="grv_margin">
+            <div class="container-fluid">
+                <div class="row first_row_margin">
+                    <div class="col-lg-4">
+                        <div class="page-header-title">
+                            <i class="fas fa-users"></i>
+                            <h5>Designations</h5>
+                            <p class="heading_Bottom"> Complete list of designations</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="page-header-breadcrumb">
+                            <div class="buttons" style="text-align:right;margin:4px;">
+
+                                <a href="{{ url('Designation/create') }}"><button type="button"
+                                        class="btn btn-success btn_new"><i class="fas fa-plus mr-2"></i>Add New</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="container-fluid bg-white mt-2 mb-5 border_radius box">
+                    <div class="row">
+                        <div class="col-md-12 mt-3 mb-3">
+
+                            <div class="container-fluid mt-3">
+                                <form action="" method="GET" id="user-search">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <label for="client_id">Slip No</label>
+                                            <fieldset>
+                                                <div class="input-group client_margin">
+                                                    <span class="input-group-addon bg-primary border-primary white"
+                                                        id="basic-addon7"
+                                                        style="width: 43px;display: flex;justify-content: center;align-items: center;font-size: 23px;color: white;background-color: #4f81a4 !important;border: #4f81a4;"><i
+                                                            class="fas fa-briefcase"></i></span>
+                                                    <input type="text" value="{{ Request::get('name') }}" name="name"
+                                                        class="form-control" id="file_no"
+                                                        placeholder="Enter  Slip No">
+                                                </div>
+                                            </fieldset>
+                                        </div>
+                                       <div class="col-md-3 mb-3 px-3">
+                                       <label for="enabled" class="yash_star"style="margin-bottom: 0px;">Slip No. </label>
+                                       <select class="form-control client_margin fstdropdown-select" id="enabled" name="slip_no" required>
+                                        
+                                      </select>
+                                       </div>
+
+
+                                        <div class="col-md-4 mb-3 px-3">
+                                            <label></label>
+                                            <input style="margin-top:23px" type="submit" name="find" value="find" class="btn btn-success">
+                                            <input style="margin-top:23px" type="submit" name="export_to_excel" value="Export To Csv" class="btn btn-primary">
+                                            <input style="margin-top:23px" type="submit" name="export_to_pdf" value="Export To PDF" class="btn btn-info">
+                                        </div>
+
+                                    </div>
+
+                                </form>
+
+
+                            </div>
+                            <hr class="border-dark bold">
+                            <div id="hide_2" class="table-responsive">
+                                <!--<div id="toolbar">-->
+                                <!--  <select class="form-control">-->
+                                <!--    <option value="">Export Basic</option>-->
+                                <!--    <option value="all">Export All</option>-->
+                                <!--    <option value="selected">Export Selected</option>-->
+                                <!--  </select>-->
+                                <!--</div>-->
+
+<!--                                 <table id="table" data-toggle="table" data-search="true" data-filter-control="true"
+                                    data-show-export="true" data-show-refresh="true" data-show-toggle="true"
+                                    data-pagination="true" data-toolbar="#toolbar">
+                                    <thead>
+                                        <tr>
+                                            <th data-field="state" data-checkbox="true"></th>
+                                            <th data-field="date23" data-sortable="true">Id</th>
+
+                                            <th data-field="date" data-sortable="true">Designation Name</th>
+
+                                            <th data-field="note" data-sortable="true">Description</th>
+
+                                            <th data-field="note13" data-sortable="true">Action</th>
+                                        </tr>
+                                    </thead>
+
+                                </table> -->
+                            </div>
+                        </div>
+
+                    </div>
+                    {{-- <svg xmlns="" version="1.1">
+                        <defs>
+                            <filter id="goo">
+                                <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+                                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7"
+                                    result="goo"></feColorMatrix>
+                                <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+                            </filter>
+                        </defs>
+                    </svg> --}}
+                    <!-- Close Row -->
+                </div>
+                <!-- Close Container -->
+            </div>
+        @endsection
