5a870d4e2d038c36274b25f07ed29f8b84b8e4ed
unknown <ks765005@gmail.com>
2023-01-30 19:21:29 +0530

    changes

--- app/Models/EntryMast.php
+++ app/Models/EntryMast.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\EntryLogs;
-use App\models\VehicleMast;
+use App\Models\VehicleMast;
 use App\Models\VendorMast;
 use Auth;
 use DB;
