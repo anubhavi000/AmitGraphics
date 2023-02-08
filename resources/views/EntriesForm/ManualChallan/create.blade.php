0688c5a8f695af69f3a58c2c7b7dbfd22746f94b
root <root@server07.manacle.dl3.da>
2023-02-08 16:56:54 +0530

    changes

--- resources/views/EntriesForm/ManualChallan/create.blade.php
+++ resources/views/EntriesForm/ManualChallan/create.blade.php
@@ -51,37 +51,14 @@
         <a class="btn btn-link btn-primary" data-toggle="collapse" data-target="#collapseExample" aria-expanded="true" aria-controls="collapseExample" style="margin-top: 10px;">        
         <i class="fa" aria-hidden="true"></i></a>            
       </div>
-<<<<<<< HEAD
-       <div class="col-md-6" style="text-align:right;">
-                  <a class="btn btn-link btn-primary" data-toggle="collapse" data-target="#collapseExample" aria-expanded="true" aria-controls="collapseExample" style="margin-top: 10px;">        
-                  <i class="fa" aria-hidden="true"></i></a>            
-                  </div>
-                </div>
-      <hr class="border-dark bold">
-   <div class="form-row mt-3 mb-3 collapse show" id="collapseExample">
-   <div class="col-md-3">
-     <label for="department_Name" class="yash_star">  Slip No. </label>
-     <input type="text" name="slip_no" onblur="checkslipduplicate(this.value)" value="{{!empty($data['slip_no']) ? $data['slip_no']  : ''}}" id="main_slip_no" class="form-control " placeholder="Enter Slip No. Here" >
-   </div>
-    <div class="col-md-3">
-      <label class="form-label">Vehicle</label>
-      <select name = "vehicle"  class="fstdropdown-select" id="vehicle" onchange="get_pass_wt(this.value)" required="true">
-          <option value="">Select</option>
-          @if(!empty($vehicles))
-            @foreach($vehicles as $key => $value)
-              <option value="{{$key}}">{{$value}}</option>
-            @endforeach
-          @endif
-      </select>
-=======
->>>>>>> 1e89553277cb7976cf9e82c8f40a97984a72d5f8
+
     </div>
     <hr class="border-dark bold">
     <div class=" mt-3 mb-3 collapse show" id="collapseExample">
         <div class="form-row mt-3 ">
             <div class="col-md-3">
                <label for="department_Name" class="yash_star">  Slip No. </label>
-               <input type="text" name="slip_no" value="{{!empty($data['slip_no']) ? $data['slip_no']  : ''}}" id="main_slip_no" class="form-control " placeholder="Enter Slip No. Here" >
+               <input type="text" name="slip_no" onblur="checkslipduplicate(this.value)" value="{{!empty($data['slip_no']) ? $data['slip_no']  : ''}}" id="main_slip_no" class="form-control " placeholder="Enter Slip No. Here" >
              </div>
             <div class="col-md-3">
               <label class="form-label">Vehicle</label>
@@ -169,11 +146,7 @@
         </div>
         <div class="form-row mt-3 ">
 
-<<<<<<< HEAD
-    <div class="col-md-3 ">
-        <label for="description">Vehicle Pass WT</label>
-        <input readonly="true" type="text" name="vehicle_pass" id="vehicle_pass" onkeyup="calculateexcessweight()" required="true" placeholder ="Enter Vehicle Pass WT"  class="form-control ">
-    </div>
+  
     <div class="col-md-3">
       <label class="form-label">Driver</label>
       <input type="text" class="form-control" name="driver" id="driver" placeholder="Enter Driver Name" >
@@ -192,21 +165,8 @@
         
       </textarea>
     </div>
-=======
-            <div class="col-md-3">
-              <label class="form-label">Driver</label>
-              <input type="text" class="form-control" name="driver" id="driver" placeholder="Enter Driver Name" >
-            </div>
-            <div class="col-md-3">
-              <label> Out Date And Time </label>
-              <input type="text" id="generationdate" name="generation_time" class="form-control datepicker" required="true"  placeholder="Loading Date time">
-            </div>    
-            <div class="col-md-4">
-              <label>Remarks</label>
-              <textarea name="remarks" placeholder="Remarks" class="form-control"></textarea>
-            </div>
         </div>
->>>>>>> 1e89553277cb7976cf9e82c8f40a97984a72d5f8
+
         <div id="infodiv" class="col-md-3">
             @if(!empty($selected_vendor))
             <label class="form-label">Vendor Details</label>
